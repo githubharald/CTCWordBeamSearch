6cf355b1ce31d5bc6ea2ef9805be60a32b238961
Harald Scheidl <harald_scheidl@hotmail.com>
2018-09-01 13:37:18 +0200

    parallel decoding of a batch

--- cpp/proj/buildTF.sh
+++ cpp/proj/buildTF.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 
-TF_VERSION=$(python3 -c "import tensorflow as tf ;  print(tf.__version__)")
+# check if parallel decoding is enabled
+if [ "$1" == "PARALLEL" ];
+then
+	echo "Parallel decoding with $2 threads"
+	PARALLEL="-DWBS_PARALLEL -DWBS_THREADS=$2"
+else
+	echo "Single-threaded decoding"
+	PARALLEL=""
+fi
 
+
+# get and print TF version
+TF_VERSION=$(python3 -c "import tensorflow as tf ;  print(tf.__version__)")
 echo "Your TF version is $TF_VERSION"
 echo "TF versions 1.3.0, 1.4.0, 1.5.0 and 1.6.0 are tested"
 
@@ -14,7 +25,7 @@ if [ "$TF_VERSION" == "1.3.0" ]; then
 
 	TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 
-	g++ -Wall -O2 --std=c++11 -shared -o TFWordBeamSearch.so ../src/TFWordBeamSearch.cpp ../src/main.cpp ../src/WordBeamSearch.cpp ../src/PrefixTree.cpp ../src/Metrics.cpp ../src/MatrixCSV.cpp ../src/LanguageModel.cpp ../src/DataLoader.cpp ../src/Beam.cpp -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 -I$TF_INC 
+	g++ -Wall -O2 --std=c++11 -shared -o TFWordBeamSearch.so ../src/TFWordBeamSearch.cpp ../src/main.cpp ../src/WordBeamSearch.cpp ../src/PrefixTree.cpp ../src/Metrics.cpp ../src/MatrixCSV.cpp ../src/LanguageModel.cpp ../src/DataLoader.cpp ../src/Beam.cpp -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 $PARALLEL -I$TF_INC 
 
 
 # compile it for TF1.4
@@ -25,7 +36,7 @@ elif [ "$TF_VERSION" == "1.4.0" ]; then
 	TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 	TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
-	g++ -Wall -O2 --std=c++11 -shared -o TFWordBeamSearch.so ../src/TFWordBeamSearch.cpp ../src/main.cpp ../src/WordBeamSearch.cpp ../src/PrefixTree.cpp ../src/Metrics.cpp ../src/MatrixCSV.cpp ../src/LanguageModel.cpp ../src/DataLoader.cpp ../src/Beam.cpp -D_GLIBCXX_USE_CXX11_ABI=0 -fPIC -I$TF_INC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework
+	g++ -Wall -O2 --std=c++11 -shared -o TFWordBeamSearch.so ../src/TFWordBeamSearch.cpp ../src/main.cpp ../src/WordBeamSearch.cpp ../src/PrefixTree.cpp ../src/Metrics.cpp ../src/MatrixCSV.cpp ../src/LanguageModel.cpp ../src/DataLoader.cpp ../src/Beam.cpp -D_GLIBCXX_USE_CXX11_ABI=0 $PARALLEL -fPIC -I$TF_INC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework
 
 # all other versions (tested for: TF1.5 and TF1.6)
 else
@@ -35,6 +46,6 @@ else
 	TF_LFLAGS=( $(python3 -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
 
-	g++ -Wall -O2 --std=c++11 -shared -o TFWordBeamSearch.so ../src/TFWordBeamSearch.cpp ../src/main.cpp ../src/WordBeamSearch.cpp ../src/PrefixTree.cpp ../src/Metrics.cpp ../src/MatrixCSV.cpp ../src/LanguageModel.cpp ../src/DataLoader.cpp ../src/Beam.cpp -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -D_GLIBCXX_USE_CXX11_ABI=0
+	g++ -Wall -O2 --std=c++11 -shared -o TFWordBeamSearch.so ../src/TFWordBeamSearch.cpp ../src/main.cpp ../src/WordBeamSearch.cpp ../src/PrefixTree.cpp ../src/Metrics.cpp ../src/MatrixCSV.cpp ../src/LanguageModel.cpp ../src/DataLoader.cpp ../src/Beam.cpp -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -D_GLIBCXX_USE_CXX11_ABI=0 $PARALLEL
 
 fi
--- cpp/src/TFWordBeamSearch.cpp
+++ cpp/src/TFWordBeamSearch.cpp
@@ -5,6 +5,7 @@
 #include <cctype>
 #include <memory>
 #include <exception>
+#include <thread>
 #include <cstddef>
 #include <stdint.h>
 #include "MatrixTensor.hpp"
@@ -34,14 +35,16 @@ REGISTER_OP("WordBeamSearch")
 using namespace tensorflow;
 
 
+
+
 // custom TF op
 class TFWordBeamSearch : public OpKernel 
 {
 private:
 	std::shared_ptr<LanguageModel> m_lm;
-	size_t m_beamWidth=0;
-	size_t m_numChars=0;
-	LanguageModelType m_lmType=LanguageModelType::Words;
+	size_t m_beamWidth = 0;
+	size_t m_numChars = 0;
+	LanguageModelType m_lmType = LanguageModelType::Words;
 
 public:
 	// CTOR
@@ -49,9 +52,9 @@ public:
 	:OpKernel(context) 
 	{
 		// read beam width
-		int64 beamWidth64=0;
+		int64 beamWidth64 = 0;
 		OP_REQUIRES_OK(context, context->GetAttr("beamWidth", &beamWidth64));
-		m_beamWidth=static_cast<size_t>(beamWidth64);
+		m_beamWidth = static_cast<size_t>(beamWidth64);
 		
 		// read type of language model
 		std::string strLmType;
@@ -59,21 +62,21 @@ public:
 		
 		// map string to enum (default is Words)
 		std::transform(strLmType.begin(), strLmType.end(), strLmType.begin(), tolower);
-		if(strLmType=="words")
+		if(strLmType == "words")
 		{
-			m_lmType=LanguageModelType::Words;
+			m_lmType = LanguageModelType::Words;
 		}
-		else if(strLmType=="ngrams")
+		else if(strLmType == "ngrams")
 		{
-			m_lmType=LanguageModelType::NGrams;
+			m_lmType = LanguageModelType::NGrams;
 		}
-		else if(strLmType=="ngramsforecast")
+		else if(strLmType == "ngramsforecast")
 		{
-			m_lmType=LanguageModelType::NGramsForecast;
+			m_lmType = LanguageModelType::NGramsForecast;
 		}
-		else if(strLmType=="ngramsforecastandsample")
+		else if(strLmType == "ngramsforecastandsample")
 		{
-			m_lmType=LanguageModelType::NGramsForecastAndSample;
+			m_lmType = LanguageModelType::NGramsForecastAndSample;
 		}
 		else
 		{
@@ -81,7 +84,7 @@ public:
 		}
 
 		// read lm smoothing value	
-		float lmSmoothing=0.0;
+		float lmSmoothing = 0.0f;
 		OP_REQUIRES_OK(context, context->GetAttr("lmSmoothing", &lmSmoothing));
 
 		// read corpus (utf8)
@@ -97,14 +100,14 @@ public:
 		OP_REQUIRES_OK(context, context->GetAttr("wordChars", &wordChars));
 
 		// create language model
-		m_lm=std::make_shared<LanguageModel>(corpus, chars, wordChars, m_lmType, lmSmoothing);
+		m_lm = std::make_shared<LanguageModel>(corpus, chars, wordChars, m_lmType, lmSmoothing);
 
 		// query number of chars (may be different to chars.size()) to check tensor shape
-		m_numChars=m_lm->getAllChars().size();
+		m_numChars = m_lm->getAllChars().size();
 
 		// check string sizes now and the mat size later in the Compute method
-		const size_t numWordChars=m_lm->getWordChars().size();
-		if(!(numWordChars>0 && numWordChars<m_numChars))
+		const size_t numWordChars = m_lm->getWordChars().size();
+		if(!(numWordChars > 0 && numWordChars < m_numChars))
 		{
 			throw std::invalid_argument("wordChars must contain at least one character and at least one character less than chars: 0<len(wordChars)<len(chars)");
 		}
@@ -112,46 +115,121 @@ public:
 	}
 
 
+#ifdef WBS_PARALLEL
+	// split batch into work groups
+	std::vector<std::vector<size_t>> splitWork(size_t numThreads, size_t batchSize)
+	{
+		std::vector<std::vector<size_t>> res(numThreads);
+
+		size_t div = batchSize / numThreads;
+		size_t rem = batchSize % numThreads;
+		size_t ctrF = 0;
+		size_t ctrB = batchSize - 1;
+		for(size_t th = 0; th < numThreads; ++th)
+		{
+			if(rem > 0)
+			{
+				res[th].push_back(ctrB);
+				ctrB--;
+				rem--;
+			}
+
+			for(size_t i = 0; i < div; ++i)
+			{
+				res[th].push_back(ctrF);
+				ctrF++;
+			}
+		}
+
+		return res;
+	}
+
+
+	// do the work for a work group
+	template<class U, class V>
+	void doWork(const U& inputMapped, V& outputMapped, size_t maxT, size_t maxC, size_t threadIndex, const std::vector<std::vector<size_t>>& workIndices)
+	{
+		const std::vector<size_t>& indices = workIndices[threadIndex];
+		const size_t blank = maxC - 1;
+		for(auto idx : indices)
+		{
+			// wrapper around Tensor
+			MatrixTensor<decltype(inputMapped)> mat(inputMapped, idx, maxT, maxC);
+
+			// apply decoding algorithm to batch element 
+			const std::vector<uint32_t> decoded = wordBeamSearch(mat, m_beamWidth, m_lm, m_lmType);
+
+			// write to output tensor
+			for(size_t t = 0; t < maxT; ++t)
+			{
+				outputMapped(idx, t) = t < decoded.size() ? decoded[t] : blank;
+			}
+
+		}
+	}
+#endif
+
+
+	
 	// computation in TF graph
 	void Compute(OpKernelContext* context) override 
 	{
 		// input: TxBxC, float32
 		const Tensor& inputTensor = context->input(0);
-		const auto inputShape=inputTensor.shape();
-		const auto T=inputShape.dim_size(0);
-		const auto B=inputShape.dim_size(1);
-		const auto C=inputShape.dim_size(2);
-		const size_t blank=C-1;
-
+		const auto inputShape = inputTensor.shape();
+		const auto maxT = inputShape.dim_size(0);
+		const auto maxB = inputShape.dim_size(1);
+		const auto maxC = inputShape.dim_size(2);
+		
 		// check tensor size
-		if(static_cast<size_t>(C)!=m_numChars+1)
+		if(static_cast<size_t>(maxC) != m_numChars + 1)
 		{
 			throw std::invalid_argument("the number of characters (chars) plus 1  must equal dimension 2 of the tensor (mat)");
 		}
 
 		// input tensor
-		auto inputMapped=inputTensor.tensor<float,3>();
+		const auto inputMapped = inputTensor.tensor<float, 3>();
 
 		// output: BxT, int32
 		Tensor* outputTensor = nullptr;
-		OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape({B, T}), &outputTensor));
-		auto outputMapped=outputTensor->tensor<int32,2>();
+		OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape({maxB, maxT}), &outputTensor));
+		auto outputMapped = outputTensor->tensor<int32, 2>();
 
+#ifdef WBS_PARALLEL
+		// split work into number of threads
+		const size_t numThreads = WBS_THREADS;
+		const auto workIndices = splitWork(numThreads, maxB);
+
+		// start threads
+		std::vector<std::thread> workers;
+		for(size_t th = 0; th < numThreads; ++th)
+		{
+			workers.push_back(std::thread([&, th](){doWork(inputMapped, outputMapped, maxT, maxC, th, workIndices);}));
+		}
+
+		// wait until all threads finished the work
+		for(auto& w : workers)
+		{
+			w.join();
+		}
+#else
 		// go over all batch elements
-		for(int b=0; b<B; ++b)
+		const size_t blank = maxC - 1;
+		for(int b = 0; b < maxB; ++b)
 		{
 			// wrapper around Tensor
-			MatrixTensor<decltype(inputMapped)> mat(inputMapped, b, T, C);
+			MatrixTensor<decltype(inputMapped)> mat(inputMapped, b, maxT, maxC);
 
 			// apply decoding algorithm to batch element 
-			const std::vector<uint32_t> decoded=wordBeamSearch(mat, m_beamWidth, m_lm, m_lmType);
+			const std::vector<uint32_t> decoded = wordBeamSearch(mat, m_beamWidth, m_lm, m_lmType);
 
 			// write to output tensor
-			for(int t=0; t<T; ++t)
+			for(int t = 0; t < maxT; ++t)
 			{
-				outputMapped(b, t)=t<static_cast<int>(decoded.size()) ? decoded[t] : blank;
+				outputMapped(b, t) = t < static_cast<int>(decoded.size()) ? decoded[t] : blank;
 			}
 		}
+#endif
 	}
 };
 
