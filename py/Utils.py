514659532deeb6c4449aa8090e3755aa45dad7b7
Harald Scheidl <harald_scheidl@hotmail.com>
2019-06-08 14:31:56 +0200

    reformat code

--- py/Utils.py
+++ py/Utils.py
@@ -1,25 +1,26 @@
-from __future__ import print_function
 from __future__ import division
-import sys
+from __future__ import print_function
+
 import csv
+import sys
 
 
 def redirectToFile():
-	sys.stdout=open('out.txt','w+')
-	
-	
+    sys.stdout = open('out.txt', 'w+')
+
+
 def flushToFile():
-	sys.stdout.flush()
-	
+    sys.stdout.flush()
+
 
 class CSVWriter:
-	"log to csv file"
-	def __init__(self):
-		self.file=open('out.csv', 'w+')
-		self.writer=csv.writer(self.file, lineterminator='\n')
-		
-	def write(self, line):
-		line=[x.encode('ascii', 'replace') for x in line] # map to ascii if possible (for py2 and windows)
-		self.writer.writerow(line)
-		self.file.flush()
-		
+    "log to csv file"
+
+    def __init__(self):
+        self.file = open('out.csv', 'w+')
+        self.writer = csv.writer(self.file, lineterminator='\n')
+
+    def write(self, line):
+        line = [x.encode('ascii', 'replace') for x in line]  # map to ascii if possible (for py2 and windows)
+        self.writer.writerow(line)
+        self.file.flush()
