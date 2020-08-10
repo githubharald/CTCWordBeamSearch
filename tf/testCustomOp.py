98d08c4355a570c3afcedcf599bd1994ce2de448
Harald Scheidl <harald_scheidl@hotmail.com>
2020-08-10 18:32:10 +0200

    script paths, chmod

--- tf/testCustomOp.py
+++ tf/testCustomOp.py
@@ -15,7 +15,7 @@ def testCustomOp(feedMat, corpus, chars, wordChars):
 	sess.run(tf.global_variables_initializer())
 
 	# load custom TF op
-	word_beam_search_module = tf.load_op_library('../cpp/proj/TFWordBeamSearch.so')
+	word_beam_search_module = tf.load_op_library('../cpp/proj/tf/TFWordBeamSearch.so')
 
 	# input with shape TxBxC
 	mat=tf.placeholder(tf.float32, shape=feedMat.shape)
