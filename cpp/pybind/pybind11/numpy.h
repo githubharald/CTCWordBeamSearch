c600b2b4685745119173c67d2959f32d4e2dadcf
Boris Nikitin <aktzbn@gmail.com>
2023-01-27 11:47:52 +0300

    Fix installation on Windows

--- cpp/pybind/pybind11/numpy.h
+++ cpp/pybind/pybind11/numpy.h
@@ -27,6 +27,10 @@
 #if defined(_MSC_VER)
 #  pragma warning(push)
 #  pragma warning(disable: 4127) // warning C4127: Conditional expression is constant
+
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+
 #endif
 
 /* This will be true on all flat address space platforms and allows us to reduce the
